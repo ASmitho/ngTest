dfd8b12853693fd0e294a61a65f1d8b30b5cb447
Smith <william.b.smith@accenture.com>
2018-06-26 14:55:23 -0500

    update with tutorial

---
+++ angular-tour-of-heroes/src/app/mock_heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Dumbo' },
+  { id: 14, name: 'Celery Man' },
+  { id: 15, name: 'Rubber Duck' },
+  { id: 16, name: 'Large Building' },
+  { id: 17, name: 'Nice Old Man' },
+  { id: 18, name: 'Smart Guy' },
+  { id: 19, name: 'Higher Than Normal Temperature Guy' },
+  { id: 20, name: 'Slightly Cold Woman' }
+];
